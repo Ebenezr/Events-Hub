445fd9d7e015cc63114aa85eb8e6d4e6afe9d702
Tim254 <tim.mailu@gmail.com>
2022-05-18 20:38:16 +0300

    update cart total elements when an event is removed from cart

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -5,7 +5,19 @@ for (i = 0; i < removeEvent.length; i++) {
     button.addEventListener('click', function(event) {
         var buttonClicked = event.target
         buttonClicked.parentElement.parentElement.parentElement.remove()
-
+        updateTotal()
     })
-    function updateTotal
 }
+    function updateTotal() {
+        var cartItemContainer = document.getElementsByClassName('container')[0]
+       var cartRows = cartItemContainer.getElementsByClassName('festdetails')
+       for (i = 0; i < cartRows.length; i++) {
+           var catRow = cartRows[i]
+           var priceElement = catRow.getElementsByClassName('price')[1]
+           var quantityElement = catRow.getElementsByClassName('quantity')[0]
+           console.log(priceElement, quantityElement)
+           var price = parseFloat(priceElement.innerText.replace('Ksh.', ''))
+           console.log(price)
+       }
+
+    }
