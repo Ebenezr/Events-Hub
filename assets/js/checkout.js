aca2754347630c79d92c3d7a32f658493824f825
Tim254 <tim.mailu@gmail.com>
2022-05-18 22:41:36 +0300

    commenting out the navbar

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -6,11 +6,16 @@ for (i = 0; i < removeEvent.length; i++) {
         var buttonClicked = event.target
         buttonClicked.parentElement.parentElement.parentElement.remove()
         updateTotal()
-    })
+    }) 
+
+    var quantityInputs = getElementsByClassName('quantity')
+    for (i = 0; i < quantityInputs.length; i++)
+
 }
     function updateTotal() {
         var cartItemContainer = document.getElementsByClassName('container')[0]
        var cartRows = cartItemContainer.getElementsByClassName('festdetails')
+       var eventtotal = 0
        var total = 0
        for (i = 0; i < cartRows.length; i++) {
            var catRow = cartRows[i]
@@ -20,9 +25,11 @@ for (i = 0; i < removeEvent.length; i++) {
            var price = parseFloat(priceElement.innerText.replace('Ksh.', ''))
            var itemCount = quantityElement.value
            total = total + (itemCount * price)
-           console.log(itemCount * price)
+           eventtotal = (itemCount * price)
+
            
        }
+       document.getElementsByClassName('subtototalprice')[0].innerText = "Ksh" + eventtotal
        document.getElementsByClassName('cart-total-price')[0].innerText = "Ksh" + total
 
     }
