42d377451508a175255b3522387cb7ab00930fdf
Tim254 <tim.mailu@gmail.com>
2022-05-19 11:11:58 +0300

    add js to make cart responsive

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,11 +1,11 @@
 var removeEvent = document.getElementsByClassName('remove')
-console.log(removeEvent)
+
 for (i = 0; i < removeEvent.length; i++) {
     var button = removeEvent[i]
     button.addEventListener('click', function(event) {
         var buttonClicked = event.target
         buttonClicked.parentElement.parentElement.parentElement.remove()
-        updateTotal()
+    
     }) 
 
     // var quantityInputs = getElementsByClassName('quantity')
@@ -14,22 +14,39 @@ for (i = 0; i < removeEvent.length; i++) {
 }
     function updateTotal() {
         var cartItemContainer = document.getElementsByClassName('container')[0]
-       var cartRows = cartItemContainer.getElementsByClassName('festdetails')
+       var cartRows = cartItemContainer.getElementsByClassName('edmfest')
        var eventtotal = 0
        var total = 0
-       for (i = 0; i < cartRows.length; i++) {
+       for (var i = 0; i < cartRows.length; i++) {
            var catRow = cartRows[i]
            var priceElement = catRow.getElementsByClassName('price')[1]
            var quantityElement = catRow.getElementsByClassName('quantity')[0]
            console.log(priceElement, quantityElement)
-           var price = parseFloat(priceElement.innerText.replace('Ksh.', ''))
+           var price = parseFloat(priceElement.innerText.replace('Ksh', ''))
            var itemCount = quantityElement.value
            total = total + (itemCount * price)
            eventtotal = (itemCount * price)
 
            
        }
-       document.getElementsByClassName('subtototalprice')[0].innerText = "Ksh" + eventtotal
+       document.getElementsByClassName('subtototalprice')[0].innerText = "Ksh" + total
        document.getElementsByClassName('cart-total-price')[0].innerText = "Ksh" + total
 
     }
+// QUANTITY CHANGE
+
+
+const onQuantityChange = (event) =>{
+    const input = event.target;
+    if(isNaN(input.value) || input.value <= 0){
+        input.value = 1;
+    }
+    else{
+        updateTotal();
+    }
+}
+const quantityInput = document.getElementsByClassName("quantity");
+for(let i =0; i < quantityInput.length; i++){
+    const input = quantityInput[i];
+    input.addEventListener("change", onQuantityChange)
+}
