82654dc1a0a5d22368d8fdea264406b21fb3d99a
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2022-05-19 12:37:10 +0300

    Add a links to checkout page and single event page

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,3 +1,5 @@
-function promptCheckout(){
-    var
-}
+// const onRedirect = () =>{
+//     const button = document.getElementsByClassName("checkout");
+//     button.addEventListener("click", function(){
+//     window.location.href = "./assets/pages/signup.html"})
+// }
